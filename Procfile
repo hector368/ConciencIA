383a2ed79b345fbeb4c1f7a4edb3f7ec83eec763
hector368 <2021451031@teschi.edu.mx>
2024-12-10 21:40:32 -0600

    comandos para render

---
+++ Procfile
@@ -0,0 +1,1 @@
+web: gunicorn ConciencIA.wsgi --log-file -
--- text_analysis/settings.py
+++ text_analysis/settings.py
@@ -9,7 +9,7 @@ https://docs.djangoproject.com/en/5.1/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/5.1/ref/settings/
 """
-
+import os
 from pathlib import Path
 from .config import PERSPECTIVE_API_KEY
 
@@ -54,6 +54,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',  # Agrega aquí
 ]
 
 ROOT_URLCONF = 'text_analysis.urls'
@@ -127,6 +128,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
 STATIC_URL = 'static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
